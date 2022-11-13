c33157005b995d44e9d25409fabc85307493aae8
动感小前端 <winniringy@gmail.com>
2022-11-13 16:54:23 +0000

    feat: contenteditable 最简单的编辑器

--- packages/mint-core/src/index.ts
+++ packages/mint-core/src/index.ts
@@ -1,5 +1,30 @@
-export const VERSION = '0.0.1';
+export interface IMintEditorProps {
+  container: HTMLElement;
+  disabled?: boolean;
+}
 
-export function isOdd(n: number): boolean {
-  return n % 2 === 1;
+export class MintEditor {
+  public container: HTMLElement;
+  public disabled: boolean;
+  constructor(props: IMintEditorProps) {
+    this.container = props.container;
+    this.disabled = props.disabled === true;
+    this.initContainer();
+  }
+
+  private initContainer() {
+    if (!this.container) {
+      throw new Error('ParamsError: missing container');
+    }
+    this.container.classList.add('mint-editor-container');
+    this.container.setAttribute('contenteditable', !this.disabled);
+  }
+
+  public getContent() {
+    return this.container.innerHTML;
+  }
+
+  public setContent(html: string) {
+    this.container.innerHTML = html;
+  }
 }
